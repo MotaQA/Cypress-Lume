d64f629a8127603c3a41a0c5b5abe5f95dbd0269
Danilo Mota <danilo.dev14@gmail.com>
2025-03-14 02:19:00 -0300

    Combo Box Added

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -14,9 +14,10 @@ const selectorsList = {
   middleNameField: "[name='middleName']",
   lastNameField: "[name='lastName']",
   genericField: ".oxd-input--active",
-  dateField: "[placeholder='dd-mm-yyyy']",
+  dateField: "[placeholder='yyyy-mm-dd']",
   dateCloseButton: ".--close",
   submitButton: "[type='submit']",
+  genericComboBox: ".oxd-select-text--after",
 }
 
   it.only('User Info Update - Success', () => {
@@ -30,15 +31,19 @@ const selectorsList = {
     cy.get("[name='firstName']").clear().type("Teste First Name")
     cy.get("[name='middleName']").clear().type("Teste Middle Name")
     cy.get("[name='lastName']").clear().type("Teste Last Name")
-    cy.get(selectorsList.genericField).eq(3).clear().type("Id Test")
-    cy.get(selectorsList.genericField).eq(4).clear().type("Other Id Test")
-    cy.get(selectorsList.genericField).eq(5).clear().type("Drivers License Number Test")
-    cy.get(selectorsList.dateField).eq(0).clear().type("15-10-2004")
+    cy.get(selectorsList.genericField).eq(4).clear().type("Id Test")
+    cy.get(selectorsList.genericField).eq(5).clear().type("Other Id Test")
+    cy.get(selectorsList.genericField).eq(6).clear().type("Drivers License Number Test")
+    cy.get(selectorsList.dateField).should('exist').eq(0).clear().type("15-10-2004")
     cy.get(selectorsList.dateCloseButton).click()
     cy.get(selectorsList.dateField).eq(1).clear().type("08-11-2015")
     cy.get(selectorsList.dateCloseButton).click()
+    cy.get(selectorsList.genericComboBox).eq(0).click()
+    cy.get(':nth-child(36) > span').click()
+    cy.get(selectorsList.genericComboBox).eq(1).click()
+    cy.get('.oxd-select-dropdown > :nth-child(2) > span').click()
     cy.get(selectorsList.submitButton).eq(0).click()
-    cy.get('body').should('contain', 'Successfully Updated')
+    //cy.get('body').should('contain', 'Successfully Updated')
 
   })
   it('Login - Fail', () => {
